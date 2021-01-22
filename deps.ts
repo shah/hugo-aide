61abd85866e37ec0f405480df85ffc6f5afd4662
Shahid N. Shah <shahid@shah.org>
2021-01-22 12:01:32 -0500

    fix: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export * as encodingTOML from "https://deno.land/std@0.83.0/encoding/toml.ts";
-export * as encodingYAML from "https://deno.land/std@0.83.0/encoding/yaml.ts";
-export * as colors from "https://deno.land/std@0.83.0/fmt/colors.ts";
-export * as fs from "https://deno.land/std@0.83.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.83.0/path/mod.ts";
+export * as encodingTOML from "https://deno.land/std@0.84.0/encoding/toml.ts";
+export * as encodingYAML from "https://deno.land/std@0.84.0/encoding/yaml.ts";
+export * as colors from "https://deno.land/std@0.84.0/fmt/colors.ts";
+export * as fs from "https://deno.land/std@0.84.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.84.0/path/mod.ts";
 export * as docopt from "https://denopkg.com/Eyal-Shalev/docopt.js@v1.0.5/src/docopt.ts";
 export * as govnSvcImport from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.11/import.ts";
 export * as govnSvcVersion from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.11/version.ts";
